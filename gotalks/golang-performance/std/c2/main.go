53f9cfd7fde383f82077f2a25204192e28d0bcb1
gedi <gediminas.morkevicius@gmail.com>
2017-01-25 10:27:35 +0200

    adds http server samples to recent gotalk slides

--- gotalks/golang-performance/std/c2/main.go
+++ gotalks/golang-performance/std/c2/main.go
@@ -13,7 +13,6 @@ type Mux struct {
 
 func (m *Mux) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	const users = "/v1/users/"
-
 	switch {
 	case req.URL.Path == "/v1":
 		m.docs(w, req)
@@ -23,15 +22,12 @@ func (m *Mux) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		http.NotFoundHandler().ServeHTTP(w, req)
 	}
 }
-
 func (m *Mux) docs(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "v1 documentation")
 }
-
 func (m *Mux) users(w http.ResponseWriter, req *http.Request, id string) {
 	fmt.Fprintf(w, "users: %s", id)
 }
-
 func main() {
 	log.Fatal(http.ListenAndServe(":8080", &Mux{}))
-}
+} //end
